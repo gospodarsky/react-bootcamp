3bc717415e52abb3511eb10ef78a24c153f84efd
Arty <art.gospodarsky@gmail.com>
2023-04-29 23:55:48 -0400

    Add SearchForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Counter } from './Counter';
+import { SearchForm } from './SearchForm';
 
 export const App = () => (
-  <>
-    <Counter />
-  </>
+  <main className='h-screen w-screen bg-gradient-to-b from-gray-600 to-gray-500'>
+    <div className='container mx-auto'>
+      <SearchForm onSearch={(value) => { console.log(value); }} query="Comedy" />
+    </div>
+  </main>
 );
---
+++ src/SearchForm.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export const SearchForm = ({ onSearch, query }: { onSearch: (query: string) => void, query: string }) => {
+  const [value, setQuery] = useState(query);
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      onSearch(value);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <div className="flex justify-center pt-20">
+      <input
+        type="text"
+        className="bg-gray-500 text-4xl rounded-xl mr-8 text-gray-200 p-4 outline-none focus:outline-gray-200 focus:outline-4"
+        defaultValue={value}
+        placeholder="..."
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
+      />
+      <button
+        className="text-4xl bg-gray-800 text-gray-200 px-8 rounded-xl outline-none focus:outline-4 focus:outline-gray-200 hover:outline-4 hover:outline-gray-200"
+        onClick={() => onSearch(value)}
+      >Search</button>
+    </div>
+  );
+};
