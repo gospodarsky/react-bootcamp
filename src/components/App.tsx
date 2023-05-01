e27674a5bc2beadfe363e513374cc6cef0a797ef
Arty <art.gospodarsky@gmail.com>
2023-05-01 00:13:08 -0400

    Add GenreSelect component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
 import { SearchForm } from './SearchForm';
+import { GenreSelect } from './GenreSelect';
+import { GENRES } from '../data';
 
 export const App = () => (
   <main className='h-screen w-screen bg-gradient-to-b from-gray-600 to-gray-500'>
     <div className='container mx-auto'>
       <SearchForm onSearch={(value) => { console.log(value); }} query="Comedy" />
+      <GenreSelect genres={GENRES} selectedGenre={"Crime"} onSelect={(genre) => { console.log(genre); }} />
     </div>
   </main>
 );
---
+++ src/data/index.ts
@@ -0,0 +1,6 @@
+export const GENRES = [
+  'Comedy',
+  'Crime',
+  'Documentary',
+  'Horror'
+];
